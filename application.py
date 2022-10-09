068819490457060a40a6a16a16cf2229d891183f
Toni <107318721+Toni2Morales@users.noreply.github.com>
2022-10-09 19:05:07 +0200

    commit1

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import pickle
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Bienvenido, esta es mi api para predecir el sentimiento de un tweet\n consulta el endpoint /sentiment y envía el texto en el cuerpo de la petición en forma de array o lista dentro del valor 'texto' para predecir el sentimiento."
+
+@app.route("/sentiment", methods=['GET'])
+def predecir():
+    dato = request.get_json()["texto"]
+    with open("sentiment_model", "rb") as f:
+        modelo = pickle.load(f)
+    prediccion = modelo.predict(dato)
+    resultado = []
+    for x in prediccion: 
+        if x ==1 :
+            resultado.append("El texto es positivo")
+        elif x == 0:
+            resultado.append("El texto es negativo")
+    return str(prediccion)
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
